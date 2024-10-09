abbb3976ca8aa64d7f6ee93293946e1b35764ba8
ThiagoSilva15 <thi181285@gmail.com>
2024-10-09 20:06:09 -0300

    Update EX07.py

--- Exercício 07/EX07.py
+++ Exercício 07/EX07.py
@@ -1,14 +1,13 @@
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 
-
 def conectar_mongodb():
     try:
         # Conectar ao MongoDB local na porta padrão 27017
         cliente = MongoClient('mongodb://localhost:27017/')
 
         # Verificar a conexão
-        cliente.admin.command('ismaster')
+        cliente.admin.command('ping')
 
         print("Conexão bem-sucedida ao MongoDB!")
         return cliente
@@ -16,28 +15,23 @@ def conectar_mongodb():
         print("Falha ao conectar ao MongoDB. Verifique se o servidor está rodando.")
         return None
 
-
 def listar_bancos_de_dados(cliente):
     if cliente is not None:
-        # Obter a lista de bancos de dados
         dbs = cliente.list_database_names()
-
-        if len(dbs) > 0:
+        if dbs:
             print("\nBancos de dados disponíveis:")
             for db in dbs:
                 print(f"- {db}")
         else:
             print("\nNão há bancos de dados disponíveis.")
     else:
-        print("Não foi possível listar os bancos de dados devido a falha na conexão.")
-
+        print("Não foi possível listar os bancos de dados devido à falha na conexão.")
 
 def main():
     cliente = conectar_mongodb()
     if cliente:
         listar_bancos_de_dados(cliente)
         cliente.close()  # Fechar a conexão
-
 
 if __name__ == "__main__":
     main()
