8f1a509000685b32866b252c6bd18e3770c90583
ThiagoSilva15 <thi181285@gmail.com>
2024-10-09 20:18:57 -0300

    Update EX11.py

--- Exercício 11/EX11.py
+++ Exercício 11/EX11.py
@@ -1,17 +1,11 @@
 def verificar_par_impar():
     try:
-        # Solicita ao usuário um número inteiro
         numero = int(input("Por favor, digite um número inteiro: "))
-
-        # Verifica se o número é par ou ímpar
         if numero % 2 == 0:
             print(f"O número {numero} é par.")
         else:
             print(f"O número {numero} é ímpar.")
-
     except ValueError:
         print("Erro: Por favor, digite um número inteiro válido.")
 
-
-# Executar o programa
 verificar_par_impar()
