75bbc7daecf6cb4dfc0c505504a81a7e6924340c
Renan Mano Otero <rmorenanmanoterocontaprincipal@gmail.com>
2024-10-09 20:11:46 -0300

    Create EX01_corrigido.py

---
+++ Exercício 01/EX01_corrigido.py
@@ -0,0 +1,11 @@
+# Matriz 3x3
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# Exibindo a matriz formatada
+print("Matriz 3x3:")
+for linha in matriz:
+    print(" | ".join(f"{elemento:^3}" for elemento in linha).rstrip())
